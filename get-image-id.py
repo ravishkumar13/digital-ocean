1c695631e37f6d759c5748be7e501c2c17551a7f
Ravish Kumar <kr.ravish07@gmail.com>
2018-07-06 13:40:48 +0530

    Update get-image-id.py
    Removed api key

--- get-image-id.py
+++ get-image-id.py
@@ -1,7 +1,7 @@
 import digitalocean as do
 import json
 
-auth_key="4ef1f11c5aea95ff064c4f3daddbfac17b0d118233613666ae64e5519f430edc"
+auth_key="API Key"
 manager=do.Manager(token=auth_key)
 
 image_id=manager.get_all_images()
