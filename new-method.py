09f769cfb5b21457fa45ec66e4d613dff66f2a0d
Ravish Kumar <kr.ravish07@gmail.com>
2018-07-06 13:42:01 +0530

    Update new-method.py

--- new-method.py
+++ new-method.py
@@ -2,7 +2,7 @@ import requests
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer 4ef1f11c5aea95ff064c4f3daddbfac17b0d118233613666ae64e5519f430edc',
+    'Authorization': 'Bearer API',
 }
 
 data = '{"name":"wp-test,"region":"blr1","size":"s-1vcpu-1gb","image":"30943092","ssh_keys":null,"backups":false,"ipv6":true,"user_data":null,"private_networking":null,"volumes": null,"tags":["web"]}'
