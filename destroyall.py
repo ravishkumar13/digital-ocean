4e170d6644dc87f3e33d0c10137ea432420295f1
Ravish Kumar <kr.ravish07@gmail.com>
2018-07-06 13:40:18 +0530

    Update destroyall.py
    Removed api key.

--- destroyall.py
+++ destroyall.py
@@ -1,6 +1,6 @@
 import digitalocean as do
 
-auth_key="4ef1f11c5aea95ff064c4f3daddbfac17b0d118233613666ae64e5519f430edc"
+auth_key="API Key"
 manager=do.Manager(token=auth_key)
 
 all_droplets=manager.get_all_droplets()
